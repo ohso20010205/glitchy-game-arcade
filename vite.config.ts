bcef88b436b02abf44246e5e42882bdd52f9f459
ohso20010205 <ohso20010205@gmail.com>
2026-04-15 16:08:11 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/glitchy-game-arcade/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
